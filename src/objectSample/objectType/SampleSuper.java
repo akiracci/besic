f9c02715ebfcf136c7d046203ea4ac729b6227ba
akiracci <akakak.1975@gmail.com>
2021-04-21 15:14:00 +0900

    20210420

--- src/objectSample/objectType/SampleSuper.java
+++ src/objectSample/objectType/SampleSuper.java
@@ -1,12 +1,15 @@
 package objectSample.objectType;
 
-
 //継承元のスーパークラス
 class SampleSuper {
-//スーパークラスのフィールド
+    //スーパークラスのフィールド
     protected String superFiled;
 
-    public SampleSuper(String superFiled){
+    public SampleSuper(String superFiled) {
+        this.superFiled = superFiled;
+    }
 
+    public String getSuperFiled() {
+        return superFiled;
     }
 }
