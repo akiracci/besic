70349e5a78ea3a4cdd2edc61aa26fb3c2cb7eec4
akiracci <akakak.1975@gmail.com>
2021-04-23 15:16:55 +0900

    20210423

--- src/selfStudy/Kaisetu1.java
+++ src/selfStudy/Kaisetu1.java
@@ -3,5 +3,19 @@ package selfStudy;
 //パッケージが宣言されて　名前「basic」がエディター（IEDA）で述される
 
 public class Kaisetu1 {
+//クラス宣言とファイル名「kaisetu1」がクラス名として（IEDA）で述される
+/* public アクセス修飾子です　パッケージ問わず参照される（参照していい設定）宣言です。ここではクラスがどの別のクラスからも読み取りできる宣言です。
+クラス宣言には　publicをつけるかつけないかのみ。　publicなしは「プライベートパッケイジ」としてパッケージ内だけの参照しかできない。
+*/
 
+
+    //メソッドの記述～～
+public static void main(String[] args) {
+    //メソッド名mainは実行用のメソッド名としてJavaに予約されているメソッド名で他に使うことはできません。
+    //public アクセス修飾子。他のクラスからの参照を設定。
+   int  testtesttest = 10 ;
+
+
+    System.out.println(testtesttest);
+}
 }
